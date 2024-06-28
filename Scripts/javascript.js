3b34312720eff13d7a22a5838f5b92c6501c123d
lolligolden <lolligolden@github.com>
2024-06-28 08:06:37 -0500

    fixing DOM display

--- Scripts/javascript.js
+++ Scripts/javascript.js
@@ -1,11 +1,13 @@
-document.getElementById("Start") = function() {playCard()};
+function playCard() {
+
 document.getElementById("Start").addEventListener('click', playCard);
 
 
 const colBINGO = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
   31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 
   63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]; 
-const squareBINGO = [B1, B2, B3, B4, B5, I1, I2, I3, I4, I5, N1, N2, N3, N4, N5, G1, G2, G3, G4, G5, O1, O2, O3, O4, O5];
+const squareBINGO = ["B1", "B2", "B3", "B4", "B5", "I1", "I2", "I3", "I4", "I5", "N1", "N2", "N3", "N4", "N5", "G1", 
+  "G2", "G3", "G4", "G5", "O1", "O2", "O3", "O4", "O5"];
 
 //Revise code to include, I,N,G,and O arrays, to pick 25 numbers. Will probably set up DOM to display square in Balls at top of page and number in square.
 let y = [];
@@ -13,7 +15,6 @@ let z = [];
 let i = 0;
 let j = 0
 
-function playCard() {
 
 for (let num of colBINGO) {
   let inArray = false;
@@ -47,11 +48,10 @@ for (let num of colBINGO) {
     displayCard();
 
     function displayCard() {
-        let square = "";
-        sqrBINGO = square;
-        let text = pickNumber;
-        document.getElementById(sqrBINGO).innerHTML = text;
-       }
+      const card = document.getElementbyId(sqrBINGO);
+      let text = pickNumber;
+      card.innerHTML = text;
+    }
     j++;
   }
 
