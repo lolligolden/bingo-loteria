c80d40ddd11fea319dd733b636f94816e2756b54
lolligolden <lolligolden@github.com>
2024-06-28 06:10:59 -0500

    Revised code to include all letters and numbers to choose from and to display number on card

--- javascript.js
+++ javascript.js
@@ -1,114 +1,57 @@
-const colB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const colI = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-const colN = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45];
-const colG = [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60];
-const colO = [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75];
-let squareB = [B1, B2, B3, B4, B5];
-let squareI = [I1, I2, I3, I4, I5];
-let squareN = [N1, N2, N3, N4, N5];
-let squareG = [G1, G2, G3, G4, G5];
-let squareO = [O1, O2, O3, O4, O5];
+const colBINGO = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+  31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 
+  63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]; 
+let squareBINGO = [B1, B2, B3, B4, B5, I1, I2, I3, I4, I5, N1, N2, N3, N4, N5, G1, G2, G3, G4, G5, O1, O2, O3, O4, O5];
 
 //Revise code to include, I,N,G,and O arrays, to pick 25 numbers. Will probably set up DOM to display square in Balls at top of page and number in square.
-
-
-
-
 let y = [];
-
 let z = [];
-
 let i = 0;
-
 let j = 0
 
-
-
-
-
-for (let num of colB) {
-
-
-
+for (let num of colBINGO) {
   let inArray = false;
+  let pickNumber = colBINGO[(Math.floor(Math.random() * colBINGO.length))];
+  let sqrBINGO = squareBINGO[(Math.floor(Math.random() * squareBINGO.length))];
 
-  let pickNumber = colB[(Math.floor(Math.random() * colB.length))];
+  //Choose random square from array squareBINGO
 
-  let sqrB = squareB[(Math.floor(Math.random() * squareB.length))];
-
-
-
-
-
-  //Choose random square from array squareB
-
-  if ((y.includes(sqrB)) && (y.length != 5)) {
-
+  if ((y.includes(sqrBINGO)) && (y.length != 24)) {
     inArray = true;
-
     num++;
-
   } else {
-
-    y.push(sqrB);
-
-
-
-    let x = squareB.indexOf(y[i]);
-
-    squareB.splice(x, 1);
-
+    y.push(sqrBINGO);
+    let x = squareBINGO.indexOf(y[i]);
+    squareBINGO.splice(x, 1);
     num++;
-
     x++;
-
     i++;
-
   }
 
+  //Choose random number from array colBINGO and remove from array
 
-
-  //Choose random number from array colB and remove from array
-
-  if ((z.includes(pickNumber)) && (z.length != 5) && (y.includes(sqrB))) {
-
+  if ((z.includes(pickNumber)) && (z.length != 24) && (y.includes(sqrBINGO))) {
     inArray = true;
-
   } else {
-
     z.push(pickNumber);
-
-
-
-    let w = colB.indexOf(z[j]);
-
-    colB.splice(w, 1);
-
-    console.log(sqrB, pickNumber);
-
+    let w = colBINGO.indexOf(z[j]);
+    colBINGO.splice(w, 1);
+    let text = pickNumber;
+    //console.log(sqrBINGO, pickNumber);
+    document.getElementById(text).innerHTML = text;
     j++;
-
   }
 
-
-
-  //Stop when y has 5 squares and z has 5 numbers
-
-  if ((y.length === 5) && (z.length === 5)) {
-
+  //Stop when y has 24 squares and z has 24 numbers
+  if ((y.length === 24) && (z.length === 24)) {
     break;
-
   }
-
 }
 
 //This code should be able to display the random number in the appropriate square
-function insert() {
-  text = ;
-  document.getElementById("B1").innerHTML = text;
+// function insert() {
+//   text = ;
+//   document.getElementById("B1").innerHTML = text;
+// }
 
-}
-
-
-
-insert();
+// insert();
